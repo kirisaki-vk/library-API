4bf935ae9d83752442dac12af7262b6d6cf648ad
kirisaki-vk <kirisaki.vk@gmail.com>
2023-11-15 20:02:36 +0300

    feat: added tests for database connection in Main

--- src/main/java/vk/kirisaki/Main.java
+++ src/main/java/vk/kirisaki/Main.java
@@ -1,7 +1,35 @@
 package vk.kirisaki;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Connection mainConnection;
+        String username = System.getenv("PG_USERNAME");
+        String password = System.getenv("PG_PASSWORD");
+        String databaseUrl = System.getenv("PG_DATABASE_URL");
+
+        try {
+            mainConnection = DriverManager.getConnection(
+                    String.format("jdbc:postgresql://%s", databaseUrl),
+                    username,
+                    password
+            );
+
+            System.out.println(
+                    mainConnection
+                            .createStatement()
+                            .executeQuery("SELECT * FROM book;")
+                            .next()
+            );
+        } catch (SQLException e) {
+            System.out.println(
+                    "Sorry, an error occurred during connection\n"
+                    + e.getMessage()
+            );
+        }
+
     }
 }
