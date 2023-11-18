df0bf15451557e3115887a3c1d187d3de8b665ec
Kirisaki_VK <kirisaki.vk@gmail.com>
2023-11-18 17:32:02 +0300

    fix: book table creation error

--- db_init/07-book_table.sql
+++ db_init/07-book_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS public.book (
 	id uuid NOT NULL DEFAULT gen_random_uuid(),
 	title varchar NOT NULL,
 	release_date date,
-	available boolean NOT NULL DEFAULT t,
-	topic public.topic NOT NULL DEFAULT OTHER,
+	available boolean NOT NULL DEFAULT 't',
+	topic public.topic NOT NULL DEFAULT 'OTHER',
 	author_id uuid NOT NULL,
 	CONSTRAINT book_pk PRIMARY KEY (id),
 	CONSTRAINT author_id_uq UNIQUE (author_id)
