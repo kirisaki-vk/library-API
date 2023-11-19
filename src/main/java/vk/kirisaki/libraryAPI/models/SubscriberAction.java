e0312534e0dc9c226898afa3d0bb67a424ff3a6c
Kirisaki_VK <kirisaki.vk@gmail.com>
2023-11-19 11:35:08 +0300

    feat: added missing models from database

---
+++ src/main/java/vk/kirisaki/libraryAPI/models/SubscriberAction.java
@@ -0,0 +1,6 @@
+package vk.kirisaki.libraryAPI.models;
+
+public enum SubscriberAction {
+    BOOK_BORROW,
+    BOOK_RENDER
+}
---
+++ src/main/java/vk/kirisaki/libraryAPI/models/SubscriberActivity.java
@@ -0,0 +1,22 @@
+package vk.kirisaki.libraryAPI.models;
+
+public class SubscriberActivity {
+    final Integer id;
+    private final Subsciber subsciber;
+    private final Book book;
+    private final SubscriberAction action;
+
+    public SubscriberActivity(Subsciber subsciber, Book book, SubscriberAction action) {
+        this.id = null;
+        this.subsciber = subsciber;
+        this.book = book;
+        this.action = action;
+    }
+
+    public SubscriberActivity(Integer id, Subsciber subsciber, Book book, SubscriberAction action) {
+        this.id = id;
+        this.subsciber = subsciber;
+        this.book = book;
+        this.action = action;
+    }
+}
