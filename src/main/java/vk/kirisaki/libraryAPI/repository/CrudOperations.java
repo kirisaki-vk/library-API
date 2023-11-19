b196c5e1dd453d8f248683a93bd5df9e6be71e54
Kirisaki_VK <kirisaki.vk@gmail.com>
2023-11-19 11:32:59 +0300

    feat: added findById method

--- src/main/java/vk/kirisaki/libraryAPI/repository/CrudOperations.java
+++ src/main/java/vk/kirisaki/libraryAPI/repository/CrudOperations.java
@@ -3,6 +3,7 @@ package vk.kirisaki.libraryAPI.repository;
 import java.util.List;
 
 public interface CrudOperations<T> {
+    T findById(Integer id);
     List<T> findAll();
     List<T> saveAll(List<T> toSave);
     T save(T toSave);
