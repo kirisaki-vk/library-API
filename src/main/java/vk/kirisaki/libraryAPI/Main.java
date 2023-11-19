5d03830bc66c149df4ed269cd5a83a42bc122f66
Kirisaki_VK <kirisaki.vk@gmail.com>
2023-11-19 22:22:03 +0300

    feat: finished one component test

--- src/main/java/vk/kirisaki/libraryAPI/Main.java
+++ src/main/java/vk/kirisaki/libraryAPI/Main.java
@@ -1,34 +1,55 @@
 package vk.kirisaki.libraryAPI;
 
+import vk.kirisaki.libraryAPI.models.Author;
+import vk.kirisaki.libraryAPI.models.Sex;
+import vk.kirisaki.libraryAPI.models.SubscriberAction;
+import vk.kirisaki.libraryAPI.models.User;
+import vk.kirisaki.libraryAPI.repository.AuthorCrudOperations;
+import vk.kirisaki.libraryAPI.repository.BookCrudOperations;
+import vk.kirisaki.libraryAPI.repository.SubscriberCrudOperations;
+import vk.kirisaki.libraryAPI.repository.UserCrudOperations;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Connection mainConnection;
-        String username = System.getenv("PG_USERNAME");
-        String password = System.getenv("PG_PASSWORD");
-        String databaseUrl = System.getenv("PG_DATABASE_URL");
+        // Initializing repositories
+        BookCrudOperations bookRepo = new BookCrudOperations();
+        AuthorCrudOperations authorRepo = new AuthorCrudOperations();
+        SubscriberCrudOperations subsRepo = new SubscriberCrudOperations();
 
-        try {
-            mainConnection = DriverManager.getConnection(
-                    String.format("jdbc:postgresql://%s", databaseUrl),
-                    username,
-                    password
-            );
+        // Author Crud Operations
+        System.out.println("=========== Performing author CRUD operations ===========");
+        System.out.println("Getting all authors");
+        List<Author> queriedAuthors = authorRepo.findAll();
 
-            System.out.println(
-                    mainConnection
-                            .createStatement()
-                            .executeQuery("SELECT * FROM book;")
-                            .next()
-            );
-        } catch (SQLException e) {
-            System.out.println(
-                    "Sorry, an error occurred during connection\n"
-                    + e.getMessage()
-            );
+        if(queriedAuthors == null) {
+            throw new Error("Failed to query all authors");
+        } else {
+            System.out.println("SUCCESS");
+            System.out.println(queriedAuthors);
+        }
+
+        Author toSaveAuthor = new Author("Another", "ATH40004", Sex.MALE);
+
+        System.out.println("Saving Author");
+        Author savedAuthor = authorRepo.save(toSaveAuthor);
+
+        if(savedAuthor == null) {
+            throw new Error("Failed to insert new Author");
+        } else {
+            System.out.println("SUCCESS");
+            System.out.println(toSaveAuthor);
+        }
+
+        System.out.println("Deleting Previously Created Author");
+        if(authorRepo.delete(savedAuthor) == null) {
+            throw new Error("Failed deleting Author");
+        } else {
+            System.out.println("SUCCESS");
         }
 
     }
